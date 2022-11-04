178b122416b6a538b84f0b6159132f4d6f291aa7
GibranKSR <111791263+GibranKSR@users.noreply.github.com>
2022-11-03 23:44:05 -0600

    x

---
+++ Graphs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node n1 = new Node(1);
+            Node n2 = new Node(2);
+            Node n3 = new Node(3);
+            Node n4 = new Node(4);
+
+            n1.connect(n2);
+            n1.connect(n3);
+            n3.connect(n4);
+            n4.connect(n1);
+        }
+    }
+}
