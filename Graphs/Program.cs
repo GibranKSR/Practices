bb64a1a91a10eb3aba3829d8283e02d3ad7e30d2
GibranKSR <111791263+GibranKSR@users.noreply.github.com>
2022-11-04 09:06:59 -0600

    x

--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -9,7 +9,6 @@ namespace Graphs
             Node n1 = new Node(1);
             Node n2 = new Node(2);
             Node n3 = new Node(3);
-            Node n4 = new Node(4);
 
             n1.connect(n2);
             n1.connect(n3);
